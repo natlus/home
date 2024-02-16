b62d1784ce7b3c1c690943840e84fab7f85a232e
natlus <jesper.sultan@gmail.com>
2024-02-16 15:46:32 +0100

    remove photos route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: 'home • natlus'
+};
+
 export default function Home() {
   return null;
 }
--- src/app/photos/page.tsx
+++
@@ -1,9 +0,0 @@
-import Modal from '../../components/Modal/Modal';
-
-export default function Home() {
-  return (
-    <>
-      <Modal title="Photos">Coming soon...</Modal>
-    </>
-  );
-}
