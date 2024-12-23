a396882d023da36c4208a7d18a8f45d94f12c1da
natlus <jesper.sultan@gmail.com>
2024-12-23 15:41:39 +0100

    fix

--- src/app/gift/lingon/page.tsx
+++ src/app/gift/lingon/page.tsx
@@ -2,7 +2,7 @@ import GiftCard from '@/components/giftcard/giftcard';
 
 export default function GiftLingon() {
   return (
-    <div>
+    <div className="fixed inset-0 flex items-center justify-center">
       <GiftCard name="Lingon" />
     </div>
   );
