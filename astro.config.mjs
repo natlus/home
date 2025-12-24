209a6f12395d5ad77e7e9f8f3769f1cd286e2ab9
natlus <jesper.sultan@gmail.com>
2025-12-24 16:25:43 +0100

    redesign

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,9 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 
-import mdx from "@astrojs/mdx";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()],
+  integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
   },
---
+++ src/components/nav-link.tsx
@@ -0,0 +1,22 @@
+import { navigate } from "astro:transitions/client";
+import { useHotkeys } from "react-hotkeys-hook";
+
+export default function NavLink({
+  href,
+  children,
+  shortcut,
+}: {
+  href: string;
+  children: React.ReactNode;
+  shortcut: string;
+}) {
+  useHotkeys(shortcut, () => {
+    navigate(href);
+  });
+
+  return (
+    <a href={href} className="font-mono flex flex-row gap-[2px] hover:text-orange transition-colors duration-200">
+      {children}
+    </a>
+  );
+}
