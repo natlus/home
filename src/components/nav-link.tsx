af112c1febdbf59497b6702f88e431cf06d84bec
natlus <jesper.sultan@gmail.com>
2025-12-25 18:02:57 +0100

    style active header link

--- src/components/nav-link.tsx
+++ src/components/nav-link.tsx
@@ -4,10 +4,12 @@ import { useHotkeys } from "react-hotkeys-hook";
 export default function NavLink({
   href,
   children,
+  className,
   shortcut,
 }: {
   href: string;
   children: React.ReactNode;
+  className?: string;
   shortcut: string;
 }) {
   useHotkeys(shortcut, () => {
@@ -15,7 +17,7 @@ export default function NavLink({
   });
 
   return (
-    <a href={href} className="font-mono text-primary! hover:text-orange! flex flex-row gap-[2px]">
+    <a href={href} className={`header-link font-mono flex flex-row gap-[2px] ${className}`}>
       {children}
     </a>
   );
