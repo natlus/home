baeda162aa42edf008a2a83af53204a3310cfc13
natlus <jesper.sultan@gmail.com>
2026-06-29 14:17:19 +0200

    update links

--- src/components/nav-link.tsx
+++ src/components/nav-link.tsx
@@ -5,12 +5,19 @@ export default function NavLink({
   href,
   children,
   shortcut,
+  target,
 }: {
   href: string;
   children: React.ReactNode;
   shortcut: string;
+  target?: "_blank";
 }) {
   useHotkeys(shortcut, () => {
+    if (target === "_blank") {
+      window.open(href, "_blank", "noopener,noreferrer");
+      return;
+    }
+
     navigate(href);
   });
 
@@ -18,6 +25,8 @@ export default function NavLink({
     <a
       href={href}
       className="text-primary! hover:text-orange! flex flex-row gap-[2px]"
+      target={target}
+      rel={target === "_blank" ? "noopener noreferrer" : undefined}
     >
       {children}
     </a>
